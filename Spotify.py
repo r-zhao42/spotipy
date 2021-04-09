ed37a84b7769eacfb7ee06e5d9ccebb0c1ae800d
Joanne Pan <jpan0917@gmail.com>
2021-04-08 22:01:31 -0400

    implemented remove non existing songs

--- Spotify.py
+++ Spotify.py
@@ -1,17 +1,34 @@
+"""
+Taking the user inputed playlist link to generate an array of track ids using spotipy (Spotify's API) and
+discarding the tracks that do not exist in our dataset.
+"""
+import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+# import playlist_link from user_input
 
-client_credentials_manager = SpotifyClientCredentials('6af4f7c48ecd43258dc10f18315e58ca', 'bf479de0603a41efb016107ce8148928')
+
+client_credentials_manager = SpotifyClientCredentials(
+    '6af4f7c48ecd43258dc10f18315e58ca', 'bf479de0603a41efb016107ce8148928')
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-def get_track_ids(playlist_id):
-    music_id_list = []
+df = pd.read_csv('data.csv')
+ids = set(df['id'])
+
+playlist_link = 'spotify:playlist:2MaKHEugScv6B9QUPIewYP'
+
+def get_track_ids(playlist_link):
+    playlist_id = playlist_link[17:]
+    song_id_list = []
     playlist = sp.playlist(playlist_id)
     for item in playlist['tracks']['items']:
-        music_track = item['track']
-        music_id_list.append(music_track['id'])
-    return music_id_list
+        song_track = item['track']
+        song_id_list.append(song_track['id'])
+    return(list(remove_non_existing_songs(song_id_list)))
 
-playlist_id = '1nrmeYg6bEo34nxHgaid1u'
-track_ids = get_track_ids(playlist_id)
-print(track_ids)
+
+def remove_non_existing_songs(song_id_list):
+    return(set(song_id_list).intersection(ids))
+
+print(get_track_ids(playlist_link))
+print(len(get_track_ids(playlist_link)))
