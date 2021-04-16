0540eeca97b972ac39844216599dd63cd46cde27
Ryan Zhao <rzhao42@icloud.com>
2021-04-15 17:33:42 -0700

    added documentaion, refactored some code. Everything runs the same

--- k_means_2d.py
+++ k_means_2d.py
@@ -1,3 +1,36 @@
+"""
+CSC111 Final Project: Playlist Generator
+
+Module Description
+==================
+This module was created as a copy of the k_means.py module intended to be used for testing
+the k-means algorithm on 2D datasets. This dataset was primarily used for testing the algorithm
+by graphing the state of a KMeansAlgo object at various iterations to see if the algorithm
+was working as intended.
+
+Copyright and Usage Information
+===============================
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+This file is Copyright (c) 2021 Si Yuan Zhao, Hayk Nazaryan, Cliff Zhang, Joanne Pan.
+"""
 from __future__ import annotations
 import matplotlib.pyplot as plt
 import random
